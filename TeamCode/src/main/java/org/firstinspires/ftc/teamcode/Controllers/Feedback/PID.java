6a66ea444718fde1dedd4fafc166817c3c3ed5d5
Vullarim <swdurda@gmail.com>
2025-08-26 20:53:20 -0500

    Added comments to feedback controller classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Feedback/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Feedback/PID.java
@@ -1,26 +1,35 @@
 package org.firstinspires.ftc.teamcode.Controllers.Feedback;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+/** Feedback controller with position based proportional, integral and derivative terms. */
 public class PID {
-    //Gain Vars
-    double _kp;
-    double _ki;
-    double _kd;
+    // Gains
+    double _kp; // Gain for proportional term
+    double _ki; // Gain for integral term
+    double _kd; // Gain for derivative term
 
-    //vars for calc
-    ElapsedTime timer;
-    double integral;
-    double lastError;
+    // Vars for calc
+    ElapsedTime timer; // Timer used for calculating derivative and integral
+    double integral; // Stores the current integral
+    double lastError; // Stores the position error from the previous loop
 
-    //Constructor
+    /** Constructs a {@link PID}.
+     * @param kp Gain for the proportional term.
+     * @param ki Gain for the integral term.
+     * @param kd Gain for the derivative term.
+     * */
     public PID(double kp, double ki, double kd) {
         _kp = kp;
         _ki = ki;
         _kd = kd;
         timer = new ElapsedTime();
     }
-    //runs PID calc
+
+    /** Runs the {@link PID} calculation on the arguments and returns the result.
+     * @param position Current motor position.
+     * @param reference Desired motor position.
+     * @return Motor power.
+     * */
     public double Calc(double position, double reference) {
         double error = reference - position;
         integral += error * timer.seconds();
@@ -28,7 +37,8 @@ public class PID {
         timer.reset();
         return (error * _kp) + (integral * _ki) + (derivative * _kd);
     }
-    //resets PID
+
+    /** Resets the integral and timer. */
     public void Reset() {
         integral = 0;
         timer.reset();
