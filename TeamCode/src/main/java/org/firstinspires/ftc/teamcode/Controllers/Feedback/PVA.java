b88e0e9aed28de1f00fab8bda991f989be6545d1
Vullarim <swdurda@gmail.com>
2025-08-28 18:38:30 -0500

    Added PVA feedback controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Feedback/PVA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Feedback/PVA.java
@@ -1,23 +1,36 @@
 package org.firstinspires.ftc.teamcode.Controllers.Feedback;
 
+/** Feedback controller with proportional position, velocity, and acceleration terms. */
 public class PVA {
-    //Gain Vars
-    double _kp;
-    double _kv;
-    double _ka;
+    // Gains
+    double _kp; // Gain for the position term
+    double _kv; // Gain for the velocity term
+    double _ka; // Gain for the acceleration term
 
-    //Constructor
+    /** Constructs a {@link PVA}.
+     * @param kp Gain for the position term.
+     * @param kv Gain for the velocity term.
+     * @param ka Gain for the acceleration term.
+     * */
     public PVA(double kp, double kv, double ka) {
         _kp = kp;
         _kv = kv;
         _ka = ka;
     }
-    //runs feedback calc
-    public double Calc(double motorPosition, double referencePosition, double motorVelocity, double referenceVelocity, double motorAcceleration, double referenceAcceleration) {
-        double positionError = referencePosition - motorPosition;
-        double velocityError = referenceVelocity - motorVelocity;
-        double accelerationError = referenceAcceleration -motorAcceleration;
-        return (positionError * _kp) + (velocityError * _kv) + (accelerationError * _ka);
-    }
 
+    /** Runs the {@link PVA} calculation on the arguments and returns the result.
+     * @param pos Current motor position.
+     * @param refPos Desired motor position.
+     * @param vel Current motor velocity.
+     * @param refVel Desired motor velocity.
+     * @param accel Current motor acceleration.
+     * @param refAccel Desired motor acceleration.
+     * @return Motor power.
+     * */
+    public double Calc(double pos, double refPos, double vel, double refVel, double accel, double refAccel) {
+        double posError = refPos - pos;
+        double velError = refVel - vel;
+        double accelError = refAccel - accel;
+        return (_kp * posError) + (_kv * velError) + (_ka * accelError); // Applies gains and adds the terms together
+    }
 }
